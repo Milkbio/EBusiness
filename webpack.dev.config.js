6b5125c26ec2e78d688b25cd1cc82c204f295ea3
杨国保 <yangguobao@edaijia-inc.cn>
2020-02-04 17:13:32 +0800

    删掉注释

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,8 +8,6 @@ const portfinder = require('portfinder');
 
 const baseWebpackConfig = require('./webpack.config');
 
-// baseWebpackConfig.entry.index.unshift("webpack-dev-server/client?http://localhost:8080/");
-
 const devWebpackConfig = merge(baseWebpackConfig, {
     devtool: 'cheap-module-eval-source-map',
     module: {
