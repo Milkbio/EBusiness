6b9fc29bd584fd7eee931b05f141c1d7dfc15aaf
杨国保 <yangguobao@edaijia-inc.cn>
2020-02-14 01:44:45 +0800

    首页轮播图

--- src/util/slider.js
+++ src/util/slider.js
@@ -16,41 +16,84 @@ class Slider {
         this.slideItems.width(this.slideWidth);
         this.slider.width(num * this.slideWidth);
         this.generateIndicator();
+        this.generateArrows();
         this.move();
-        this.hack();
+        this.resolveException();
     }
-    hack() {
-        $(window).on('focus', () => {
-            this.move();
-        })
-        $(window).on('blur', () => {
-            clearInterval(this.timer);
+    // 处理切换窗口时定时器的异常
+    resolveException() {
+        $(document).on('visibilitychange', () => {
+            if (document.visibilityState === 'visible') {
+                this.move();
+            } else {
+                clearInterval(this.timer);
+            }
         })
     }
+    // 根据slide的数量生成相应数量的底部小圆点，并绑定事件
     generateIndicator() {
         this.indicatorBox = $('<div class="indicator-box">');
         const indicatorWrap = $('<div class="indicator-wrap">');
+
         for (let i = 0; i < this.slideItems.length; i++) {
             const indicator = $('<div class="indicator">');
             if (i === 0) indicator.addClass('active');
+            indicator.on('click', this.indicatorClickFn.bind(this));
             indicatorWrap.append(indicator);
         }
+
         this.indicatorBox.append(indicatorWrap);
         this.el.append(this.indicatorBox)
     }
+    indicatorClickFn(e) {
+        const index = $(e.target).index();
+        this.index = index;
+        this.moveFn(index);
+    }
+    leftArrowClickFn(e) {
+        this.index--;
+        if (this.index <= 0 ) {
+            this.index = this.slideItems.length - 1;
+        }
+        this.moveFn(this.index);
+    }
+    rightArrowClickFn(e) {
+        this.index++;
+        if (this.index >= this.slideItems.length ) {
+            this.index = 0;
+        }
+        this.moveFn(this.index);
+    }
+    // 生成左右箭头，并绑定事件
+    generateArrows() {
+        const leftArrowBox = $('<div class="left-arrow-box">');
+        const leftArrow = $('<div class="iconfont icon-xiangzuo1">');
+        leftArrowBox.append(leftArrow);
+        leftArrowBox.on('click', this.leftArrowClickFn.bind(this));
 
+        const rightArrowBox = $('<div class="right-arrow-box">');
+        const rightArrow = $('<div class="iconfont icon-xiangyou1">');
+        rightArrowBox.append(rightArrow);
+        rightArrowBox.on('click', this.rightArrowClickFn.bind(this));
+
+        this.el.append(leftArrowBox);
+        this.el.append(rightArrowBox);
+    }
+    moveFn(index) {
+        this.indicatorBox.find('.indicator').removeClass('active');
+        this.indicatorBox.find('.indicator').eq(index).addClass('active');
+        this.slider.animate(
+            {marginLeft: `-${index * this.slideWidth}px`},
+            800
+        );
+    }
     move() {
         this.timer = setInterval(() => {
             this.index++;
             if (this.index >= this.slideItems.length ) {
                 this.index = 0;
             }
-            this.indicatorBox.find('.indicator').removeClass('active');
-            this.indicatorBox.find('.indicator').eq(this.index).addClass('active');
-            this.slider.animate(
-                {marginLeft: `-${this.index * this.slideWidth}px`},
-                800
-            );
+            this.moveFn(this.index);
         }, this.speed);
     }
 }
